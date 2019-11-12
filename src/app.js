20a0f33636b7bed37e138f4979134835f9eb3b7f
Alexios Markopoulos <alexmarkop@hotmail.com>
2019-11-12 20:58:57 +0200

    Module 3

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), {encoding: 'UTF8'});
 const accounts = JSON.parse(accountData);
@@ -35,8 +36,41 @@ app.get('/credit', function (req, res) {
 
 app.get('/profile', function (req, res) {
     res.render('profile', {user: users[0]});
-})
+});
+
+app.get('/transfer', function (req, res) {
+    res.render('transfer');
+});
+
+app.post('/transfer', function (req, res) {
+    const from = req.body.from;
+    const to = req.body.to;
+    const amount = req.body.amount;
+
+    accounts[from].balance -= parseInt(amount);
+    accounts[to].balance += parseInt(amount);
+    var accountsJSON = JSON.stringify(accounts);
+
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment', function (req, res) {
+    res.render('payment', {account: accounts.credit});
+});
+
+app.post('/payment', function (req, res) {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+    var accountsJSON = JSON.stringify(accounts);
+
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+
+    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+
+});
 
 app.listen(3000, function () {
     console.log('PS Project Running on port 3000!')
-})
+});
